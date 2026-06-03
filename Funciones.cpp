14a0038e5a8878354d72df790390adc9628eedbb
JeremyEGV <00202826@uca.edu.sv>
2026-06-03 12:12:58 -0600

    :memo: Funciones

---
+++ Funciones.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+//************Ejercicio sencillo****************
+/*
+int conversion(int n)
+{
+    return (n * 9 / 5) + 32;
+}
+int main()
+{
+    int a;
+    cout << "Ingrese un grado en celsius" << endl;
+    cin >> a;
+    cout << "El grado en fahrenheit es: " << conversion(a) << endl;
+    return 0;
+}
+*/
+//**********Ejercicio medio*****************
+/*
+int suma(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    int num1, num2;
+    cout << "Ingrese un numero" << endl;
+    cin >> num1;
+    cout << "Ingrese otro numero"<<endl;
+    cin >> num2;
+    cout << "La suma es: " << suma(num1, num2) << endl;
+    return 0;
+}
+*/
+//***************Ejercicio complejo********************
+/*
+bool esPar(int n)
+{
+    return n % 2 == 0;
+}
+int main()
+{
+    int numero;
+    cout << "Ingrese un numero positivo cualquiera" << endl;
+    cin >> numero;
+    if (esPar(numero))
+    {
+        cout << numero << "es par." << endl;
+    }
+    else
+    {
+        cout << numero << "no es par." << endl;
+    }
+    return 0;
+}
+*/
